6dff5bce5da580a44dc7b13a4f42c0c2b6810ac9
merei116 <zhaparkhanovamerei.16@gmailcom>
2024-08-09 21:56:01 +0500

    errors

---
+++ scripts/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+python manage.py wait_db
+python manage.py cpllectstatic --noinput
+python manage.py migrate
+
+uwsgi --socket :9000 --workers 4  --master --enable-threads --module app.wsgi
+
+
+ 
