daa1dd97348bc7c8f071b8ec213f03a3164af16b
merei116 <zhaparkhanovamerei.16@gmailcom>
2024-08-11 00:16:38 +0500

    deployment

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,10 +2,10 @@
 
 set -e
 python manage.py wait_db
-python manage.py cpllectstatic --noinput
+python manage.py collectstatic --noinput
 python manage.py migrate
 
-uwsgi --socket :9000 --workers 4  --master --enable-threads --module app.wsgi
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
 
 
  
